67feecb625ac5704f07f53b3d02f96fe894eff64
YeolJ00 <karegun@gmail.com>
2022-11-24 19:06:43 +0900

    authentication trial

--- personal-projects/m3u8-player/authenticate.php
+++ personal-projects/m3u8-player/authenticate.php
@@ -1,15 +1,15 @@
 <?php
 session_start();
 // Change this to your connection info.
-// if ( !isset($_POST['search-placeholder']) ) {
-// 	// Could not get the data that should have been sent.
-// 	exit('Please enter a code');
-// }
+if ( !isset($_GET['search-placeholder']) ) {
+	// Could not get the data that should have been sent.
+	exit('Please enter a code');
+}
 
 $code = "9377868c155e345c2a5a15a5572ce887";
 
 // Note: remember to use password_hash in your registration file to store the hashed passwords.
-if ($_POST['search-placeholder'] === $code) {
+if ($_GET['search-placeholder'] === $code) {
     // Verification success! User has logged-in!
     // Create sessions, so we know the user is logged in, they basically act like cookies but remember the data on the server.
     session_regenerate_id();
